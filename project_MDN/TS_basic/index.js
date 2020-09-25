3d04e8160d91029e739d25b09229322914d38c6a
Hong-Seon <ahs9702@daum.net>
2020-09-26 01:03:20 +0900

    First Typescript

--- project_MDN/TS_basic/index.js
+++ project_MDN/TS_basic/index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Hello";
+const sayHi = (name) => {
+    console.log(`My Name is ${name}`);
+};
+sayHi(name);
 //# sourceMappingURL=index.js.map
--- project_MDN/TS_basic/index.ts
+++ project_MDN/TS_basic/index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+const name = "Hello";
+
+const sayHi = (name) => {
+    console.log(`My Name is ${name}`)
+
+}
+sayHi(name);
+
+export {};
