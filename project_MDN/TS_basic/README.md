27324e51850305fe15fc422b26b3a104e06c3bfd
Hong-Seon <ahs9702@daum.net>
2020-09-26 22:53:55 +0900

    Setting Interface

---
+++ project_MDN/TS_basic/README.md
@@ -0,0 +1,13 @@
+# TypeScript
+
+## 1. Setting Typescirpt
+
+## 2. Using with JS function
+
+## 3. Typyes in Typescript
+- 1.Setting types
+- 2.Setting void or types
+- 3.Setting tsc-watch
+
+## 4. Sending object
+- 1.Interface system
--- project_MDN/TS_basic/src/index.ts
+++ project_MDN/TS_basic/src/index.ts
@@ -1,9 +1,24 @@
+interface Human {
+    name:string;
+    age:number;
+    gender:string;
+};
+
+const person ={
+    name:"jack",
+    age:44,
+    gender:"male"
+};
+
 const name = "Hello";
 
 const sayHi = (name:string, age:number, gender:string): void => {
     console.log(`My Name is ${name}`)
 
 }
+const sayHello = (person: Human): void => {
+    console.log(`Hello My name is ${person.name}, and ${person.age} years old, and ${person.gender}`)
+}
 sayHi(name, 24, "male");
-
+sayHello(person);
 export {};
